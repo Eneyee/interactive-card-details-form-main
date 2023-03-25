fe392155404bb55c1ac7675766d0759951a29865
Eneyee <max.aygelson@gmail.com>
2023-03-25 21:37:31 +0200

    changed error id and class

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const notifContainer = document.querySelector(".notif-container");
 const closeBtn = document.querySelector(".notif-container .close-img");
 let notifText = document.getElementById("notif-text");
+let ccNum = document.getElementById("card-number");
+let frontNumber = document.getElementById("front-number");
 
 notifText.textContent = "Hi, name's Eneyee. Could you help me fill these? I have no idea what the correct answer is."
 
@@ -14,3 +16,30 @@ const stopDisplayingNotif = () => {
 }
 
 closeBtn.addEventListener("click", stopDisplayingNotif);
+
+
+// frontNumber.addEventListener("blur", innerNumber);
+
+// function innerNumber() {
+//     if (ccNum != 0) {
+//         frontNumber.textContent = ccNum.textContent;
+//     } else {
+//         frontNumber.textContent = "0000 0000 0000 0000"
+//     }
+// }
+
+
+// ccNum.addEventListener(cardNumberValidation);
+
+// function cardNumberValidation() {
+//     var ccNum = document.getElementById("card-number").value;
+//     var ccNumRegEx = /(\d{4}[-. ]?){4}|\d{4}[-. ]?\d{6}[-. ]?\d{5}/g;
+//     var isValid = false;
+//     let errorCCNum = document.getElementById("error-ccnum");
+
+//     if (ccNumRegEx.test(ccNum)){
+//         isValid = true
+//     } else {
+//         errorText.textContent = "Please provide correct credit card number"
+//     }
+// }
