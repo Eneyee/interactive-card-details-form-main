9ba519ac564f8da276ffc366e73d06ac8b9db6d9
Eneyee <122353277+Eneyee@users.noreply.github.com>
2023-03-28 17:31:17 +0300

    added regex format for credit card input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
+//declared notification 
 const notifContainer = document.querySelector(".notif-container");
 const closeBtn = document.querySelector(".notif-container .close-img");
 let notifText = document.getElementById("notif-text");
-let ccNum = document.getElementById("card-number");
-let frontNumber = document.getElementById("front-number");
 
+//declared form and button
+const form = document.querySelector(".input-container");
+const submitBtn = document.getElementById("btn-submit");
+let inputs = document.querySelectorAll("input");
+
+//declared card holder name
+let nameCH = document.querySelector(".name-ch");
+let frontName = document.querySelector(".front-name");
+
+//declared card number display
+let ccNum = document.querySelector(".card-number");
+let frontNumber = document.querySelector(".front-number");
+
+//declared exp date
+let expMM = document.querySelector(".mm-number");
+let expYY = document.querySelector(".yy-number");
+let frontExp = document.querySelector(".front-exp");
+
+//declared cvc number
+let cvc = document.querySelector(".cvc-number");
+let backCvc = document.querySelector(".back-number");
+
+
+//notification function
 notifText.textContent = "Hi, name's Eneyee. Could you help me fill these? I have no idea what the correct answer is."
 
 setTimeout(() => {
@@ -18,28 +41,87 @@ const stopDisplayingNotif = () => {
 closeBtn.addEventListener("click", stopDisplayingNotif);
 
 
-// frontNumber.addEventListener("blur", innerNumber);
+//name display function 
+nameCH.addEventListener("keyup", (e) =>{
+    if (!e.target.value) {
+        frontName.innerHTML = "JANE APPLESEED";
+    } else {
+        frontName.innerHTML = e.target.value.toUpperCase();
+    }
+})
 
-// function innerNumber() {
-//     if (ccNum != 0) {
-//         frontNumber.textContent = ccNum.textContent;
-//     } else {
-//         frontNumber.textContent = "0000 0000 0000 0000"
-//     }
-// }
+//card number display function
+ccNum.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        frontNumber.innerText = "0000 0000 0000 0000";
+    } else {
+        const valuesOfInput = e.target.value.replaceAll(" ", "");
 
+        if (e.target.value.length > 14) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+            frontNumber.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+        } else if (e.target.value.length > 9) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+            frontNumber.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+        } else if (e.target.value.length > 4) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+            frontNumber.innerHTML = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+        } else {
+            frontNumber.innerHTML = valuesOfInput
+        }
+    }
+})
 
-// ccNum.addEventListener(cardNumberValidation);
+//exp date display function
+function checkExp(){
+    if (!expMM.value && !expYY.value) {
+        frontExp.innerHTML = "00/00";
+    }
+}
 
-// function cardNumberValidation() {
-//     var ccNum = document.getElementById("card-number").value;
-//     var ccNumRegEx = /(\d{4}[-. ]?){4}|\d{4}[-. ]?\d{6}[-. ]?\d{5}/g;
-//     var isValid = false;
-//     let errorCCNum = document.getElementById("error-ccnum");
+expMM.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        frontExp.innerHTML = "00" + "/"+ expYY.value;
+    } else if(!expYY.value) {
+        frontExp.innerHTML = e.target.value + "/" + "00";
+    } else {
+        frontExp.innerHTML = e.target.value + "/" + expYY.value;
+    }
+    checkExp();
+})
 
-//     if (ccNumRegEx.test(ccNum)){
-//         isValid = true
-//     } else {
-//         errorText.textContent = "Please provide correct credit card number"
-//     }
-// }
+expYY.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        frontExp.innerHTML = expMM.value + "/" +"00";
+    } else if(expMM.value) {
+        frontExp.innerHTML = expMM.value + "/" + e.target.value;
+    } else {
+        frontExp.innerHTML = "00" + "/" + e.target.value;
+    }
+    checkExp();
+})
+
+//cvc display function
+cvc.addEventListener("keyup", (e) => {
+    if(!e.target.value) {
+        backCvc.innerHTML = "000";
+    } else {
+        backCvc.innerHTML = e.target.value;
+    }
+})
+
+//
+const nameError = querySelector(".error-name")
+const ccNumError = querySelector(".error-ccnum")
+const ccExpCvc = querySelector(".error-exp-cvc")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    inputs.forEach((input) => {
+		if (!input.value) {
+            nameError.innerHTML = "Can't be blank";
+            ccNumError.innerHTML = "Can't be blank";
+            ccExpCvc.innerHTML = "Can't be blank";
+        }
+})
+});
